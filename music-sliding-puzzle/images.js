5eb93ba2416a46dbfbeeb23168ccd5406bca18cc
yelofirb27 <yelofirb27@msn.com>
2026-08-16 12:16:30 -0700

    Add files via upload

---
+++ music-sliding-puzzle/images.js
@@ -0,0 +1,8 @@
+// Add new puzzle images here as the year goes on.
+// Put each PNG/JPG inside the "images" folder, then add one entry below.
+window.PUZZLE_IMAGES = [
+  {
+    name: "Music Is All Around Us",
+    file: "images/music-is-all-around-us.png"
+  }
+];
