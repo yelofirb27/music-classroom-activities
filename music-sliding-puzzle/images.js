80db5f2331f784c4ec2fb32360473fefc3dbeef6
yelofirb27 <yelofirb27@msn.com>
2026-08-16 12:41:18 -0700

    Update images.js

--- music-sliding-puzzle/images.js
+++ music-sliding-puzzle/images.js
@@ -1,10 +1,13 @@
 // Add new puzzle images here as the year goes on.
 // Put each PNG/JPG inside the "images" folder, then add one entry below.
+
 window.PUZZLE_IMAGES = [
   {
     name: "Music Is All Around Us",
     file: "images/music-is-all-around-us.png"
   },
-  name: "Music Is Everything",
+  {
+    name: "Music Is Everything",
     file: "images/music-is-everything.png"
+  }
 ];
