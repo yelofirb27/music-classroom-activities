b2d82d84ed0fbe39eb6795346a2b7e1002dc3fc1
yelofirb27 <yelofirb27@msn.com>
2026-08-17 23:13:21 -0700

    Update images.js

--- music-sliding-puzzle/images.js
+++ music-sliding-puzzle/images.js
@@ -1,14 +1,14 @@
 window.PUZZLE_IMAGES = [
   {
-    name: "Music Is All Around Us",
-    file: "images/music-is-all-around-us.png"
-  },
-  {
     name: "Music Is Everything",
     file: "images/music-is-everything.png"
   },
   {
     name: "Taylor Swift – Music Always Has",
     file: "images/taylor-swift-music-quote.png"
+  },
+  {
+    name: "Pete Seeger – Wrong Notes",
+    file: "images/pete-seeger-wrong-notes.png"
   }
 ];
